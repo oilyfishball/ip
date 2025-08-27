3b4dd29cb82829560c5e4a60052493a2b78a5ed2
oilyfishball <93373760+oilyfishball@users.noreply.github.com>
2025-08-27 14:45:23 +0800

    Remove test file

--- src/main/java/mypackage/Global.java
+++ src/main/java/mypackage/Global.java
@@ -4,7 +4,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Global {
-    public static final Path FILEPATH = Paths.get(".", "test.txt");
+    public static final Path FILEPATH = Paths.get(".", "tasks.txt");
     private static String line = "-----------------------------------------------------";
     public static void printline() {
         System.out.println(line);
