fd0ee1102daebdf59718f0486e6ae99bdcac414b
oilyfishball <93373760+oilyfishball@users.noreply.github.com>
2025-08-28 00:50:47 +0800

    Comment TaskList

--- src/main/java/ackermann/TaskList.java
+++ src/main/java/ackermann/TaskList.java
@@ -8,27 +8,53 @@ import java.util.List;
 
 public class TaskList {
     private List<Task> tasks;
-    
-    public TaskList(List<Task> prompt) {
-        this.tasks = prompt;
+
+    /**
+     * Constructor for TaskList
+     * @param tasks list of tasks to keep track of
+     */
+    public TaskList(List<Task> tasks) {
+        this.tasks = tasks;
     }
 
+    /**
+     * Constructor for Tasklist
+     * Initialised by empty array list
+     * Used for first time users
+     */
     public TaskList() {
         this.tasks = new ArrayList<>();
     }
 
+    /**
+     *
+     * @return number of tasks
+     */
     public int size() {
         return this.tasks.size();
     }
 
+    /**
+     * Gets a task from the list
+     * @param i index of the task to retrieve
+     * @return A task object
+     */
     public Task get(int i) {
         return this.tasks.get(i);
     }
 
+    /**
+     * Removes a task from the list
+     * @param i index of the task to remove
+     */
     public void remove(int i) {
         this.tasks.remove(i);
     }
 
+    /**
+     * Adds a task from the list
+     * @param task Task to append to the list.
+     */
     public void add(Task task) {
         this.tasks.add(task);
     }
