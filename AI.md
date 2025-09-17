834b8f60e872a99cee96a28b76e35d232051782d
oilyfishball <93373760+oilyfishball@users.noreply.github.com>
2025-09-17 20:44:47 +0800

    Improve GUI to dark mode

--- AI.md
+++ AI.md
@@ -1,1 +1,12 @@
-Asked claude.ai on how can I include a space between image and text for gui.
+Asked Claude:
+- on how can I include a space between image and text for gui.
+- to make the dialog box a text bubble
+- to make profile picture circle
+- to make the GUI dark themed
+- to make the window resizeable
+
+Asked Gemini:
+- to generate a logo for the bot
+- to generate a styles.css script make scrollbar dark
+
+As part of A-AiAssisted
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,15 @@
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.Objects;
 
 import ackermann.Ackermann;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * A GUI for Duke using FXML.
@@ -18,11 +21,17 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         try {
-            assert true: "ok";
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
+
+            String css = Objects.requireNonNull(this.getClass().getResource("styles.css")).toExternalForm();
+            scene.getStylesheets().add(css);
             stage.setScene(scene);
+            stage.setTitle("Ackermann");
+            stage.getIcons().add(
+                    new Image(getClass().getResourceAsStream("/images/Ackermann.png"))
+            );
             fxmlLoader.<MainWindow>getController().setAckermann(duke);
             // inject the Ackermann instance
             stage.show();
